fed8c10fa651afb9e5df9d0bcb403084a7a139cf
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-08 16:08:52 +0530

    master

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -50,8 +50,6 @@ echo "the tail percent for one coin flipping $limit1 number of times is $tailPer
 
 ## usecase3
 
-## usecase3
-
 declare -A coinCount
 
 coinCount[HeadHead]=0
@@ -139,3 +137,26 @@ echo "the head head head percent for three coin flipping $limit3 number of times
 echo "the tail tail tail percent for three coin flipping $limit3 number of times is $tailTailTailPercent"
 echo "the head head tail percent for three coin flipping $limit3 number of times is $headHeadTailPercent"
 echo "the head tail tail percent for three coin flipping $limit3 number of times is $headTailTailPercent"
+
+## usecase5
+
+declare  -A Sort
+
+Sort[1]=$headPercent
+Sort[2]=$tailPercent
+Sort[3]=$headHeadPercent
+Sort[4]=$tailTailPercent
+Sort[5]=$headTailPercent
+Sort[6]=$headHeadHeadPercent
+Sort[7]=$tailTailTailPercent
+Sort[8]=$headHeadTailPercent
+Sort[9]=$headTailTailPercent
+echo "the win values are ${Sort[@]}"
+variable=${Sort[@]}
+for value in $variable
+do
+   array+=($value)
+done
+   echo "array value are ${array[@]}"
+sorting=`echo ${array[@]} | awk 'BEGIN{RS=" "} {print $1}' | sort -n `
+echo $sorting

5b6a27b471605f58f435b70e4fc9ec45cdda46f9
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-08 13:25:25 +0530

    usecase2

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-echo "welcome to flip coin combination"
-
-## usecase1
-
-Head=0
-Tail=1
-flipCoin=$((RANDOM%2))
-	if [ $flipCoin -eq $Head ]
-	then
-		echo "head"
-	else
-		echo "tail"
-	fi
-
 ## usecase2
 
 declare -A coinCount
